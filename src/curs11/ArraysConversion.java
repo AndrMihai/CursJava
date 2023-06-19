9543f5b1de0cd967d80167bca70ac317d8290211
Andrei Mihai <andrei.mihai3110@gmail.com>
2023-06-19 11:34:38 +0300

    curs 11 final

---
+++ src/curs11/ArraysConversion.java
@@ -0,0 +1,23 @@
+package curs11;
+
+import java.util.Arrays;
+import java.util.*;
+
+public class ArraysConversion {
+
+	public static void main(String[] args) {
+		
+		Integer[] numbers = {1, 2, 3, 4, 5};
+		Set<Integer> setNumbers = new HashSet<Integer>(Arrays.asList(numbers)));
+		
+		for(Integer num : setNumbers) {
+			System.out.println(num);
+		}
+		List<Integer> listNumbers = new ArrayList<Integer>(Arrays.asList(numbers));
+		System.out.println("-------------------");
+		for(Integer num : listNumbers) {
+			System.out.println(num);
+		}
+	}
+
+}
