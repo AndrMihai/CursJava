8ec32e6766983770fc5f58b48b1eb4f5c365e0bc
Andrei Mihai <andrei.mihai3110@gmail.com>
2023-06-26 10:27:07 +0300

    curs13 intermediar

--- src/curs12/TestJsonFileProcessor.java
+++ src/curs12/TestJsonFileProcessor.java
@@ -5,6 +5,17 @@ public class TestJsonFileProcessor {
 	public static void main(String[] args) {
 		JsonFileProcessor obj = new JsonFileProcessor();
 		obj.writeJsonFile();
+		obj.readJson("name");
+		obj.readJson("email");
+		obj.readJson("listaObiecte");
+		System.out.println("---------------------------------------------------------------");
+		
+		obj.updateJsonFile("email", "electronicmail");
+		System.out.println("Email after update");
+		obj.readJson("email");
+		obj.deleteFromJson("email");
+		System.out.println("email after delete");
+		obj.readJson("email");
 
 	}
 
